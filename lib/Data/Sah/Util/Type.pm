eddae45eacf96133124f9995bc46e1a7b7ebf07b
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-12-09 19:04:12 +0700

    [doc] Give more examples, explanation, as well as reference table for get_type/is_type/is_simple/is_collection/is_ref/is_numeric

--- lib/Data/Sah/Util/Type.pm
+++ lib/Data/Sah/Util/Type.pm
@@ -12,7 +12,7 @@ our @ISA       = qw(Exporter);
 our @EXPORT_OK = qw(get_type is_type is_simple is_numeric is_collection is_ref);
 
 # XXX absorb and use metadata from Data::Sah::Type::*
-my $type_metas = {
+our $type_metas = {
     all   => {scalar=>0, numeric=>0, ref=>0},
     any   => {scalar=>0, numeric=>0, ref=>0},
     array => {scalar=>0, numeric=>0, ref=>1},
@@ -141,6 +141,7 @@ sub is_ref {
  say get_type("int");                          # -> int
  say get_type("int*");                         # -> int
  say get_type([int => min=>0]);                # -> int
+ say get_type("foo");                          # -> foo (doesn't check type is known)
 
  say is_type("int*");                          # -> 1
  say is_type("foo");                           # -> 0
@@ -148,12 +149,22 @@ sub is_ref {
  say is_simple("int");                          # -> 1
  say is_simple("array");                        # -> 0
  say is_simple([any => of => ["float", "str"]); # -> 1
+ say is_simple("re");                           # -> 1
+ say is_simple("foo");                          # -> 0
 
- say is_numeric(["int", min=>0]); # -> 1
-
- say is_collection("array*"); # -> 1
+ say is_collection("array*");            # -> 1
+ say is_collection(["hash", of=>"int"]); # -> 1
+ say is_collection("str");               # -> 0
+ say is_collection("foo");               # -> 0
 
  say is_ref("code*"); # -> 1
+ say is_ref("array"); # -> 1
+ say is_ref("str");   # -> 0
+ say is_ref("foo");   # -> 0
+
+ say is_numeric(["int", min=>0]); # -> 1
+ say is_numeric("str");           # -> 0
+ say is_numeric("foo");           # -> 0
 
 
 =head1 DESCRIPTION
@@ -162,6 +173,10 @@ This module provides some secondary utility functions related to L<Sah> and
 L<Data::Sah>. It is deliberately distributed separately from the Data-Sah main
 distribution to be differentiated from Data::Sah::Util which contains "primary"
 utilities and is distributed with Data-Sah.
+
+Reference table for simple/collection/ref/numeric criteria of builtin types:
+
+# CODE: my $tm = $Data::Sah::Util::Type::type_metas; my @res; for (grep {$_ ne 'any' && $_ ne 'all'} sort keys %$tm) { push @res, {type=>$_, is_simple=>Data::Sah::Util::Type::is_simple($_) ? 1:"", is_numeric=>Data::Sah::Util::Type::is_numeric($_) ? 1:"", is_collection=>Data::Sah::Util::Type::is_collection($_) ? 1:"", is_ref=>Data::Sah::Util::Type::is_ref($_) ? 1:"" } } require Perinci::Result::Format::Lite; Perinci::Result::Format::Lite::format([200, "OK", \@res, {'table.fields'=>[qw/type is_simple is_collection is_ref is_numeric/]}], "text-pretty");
 
 
 =head1 FUNCTIONS
@@ -178,7 +193,15 @@ Return type name if type in schema is known, or undef.
 
 =head2 is_simple($sch[, \%opts]) => BOOL
 
-Simple means scalar and not a reference.
+Simple means "scalar" or can be represented as a scalar. This is currently used
+to determine if a builtin type can be specified as an argument or option value
+in command-line.
+
+This includes C<re>, C<bool>, as well as C<date> and C<duration>.
+
+If type is C<all>, then for this routine to be true all of the mentioned types
+must be simple. If type is C<any>, then for this routine to be true at least one
+of the mentioned types must be simple.
 
 Options:
 
@@ -190,11 +213,28 @@ Options:
 
 =head2 is_collection($sch[, \%opts]) => BOOL
 
+Collection means C<array> or C<hash>.
+
+If type is C<all>, then for this routine to be true all of the mentioned types
+must be collection. If type is C<any>, then for this routine to be true at least
+one of the mentioned types must be collection.
+
+=head2 is_ref($sch[, \%opts]) => BOOL
+
+"Ref" means generally a reference in Perl. But C<date> and C<duration> are not
+regarded as "ref". Regular expression on the other hand is regarded as a ref.
+
+If type is C<all>, then for this routine to be true all of the mentioned types
+must be "ref". If type is C<any>, then for this routine to be true at least one
+of the mentioned types must be "ref".
+
 =head2 is_numeric($sch[, \%opts]) => BOOL
 
 Currently, only C<num>, C<int>, and C<float> are numeric.
 
-=head2 is_ref($sch[, \%opts]) => BOOL
+If type is C<all>, then for this routine to be true all of the mentioned types
+must be numeric. If type is C<any>, then for this routine to be true at least
+one of the mentioned types must be numeric.
 
 
 =head1 SEE ALSO
